78a024a8532c15e9ed882d1c82fd38faf2f757d7
Abdul-Kalam0 <abdulkalamblycomp@gmail.com>
2026-08-01 16:54:50 +0530

    refactor(auth): make authentication cookies environment-aware

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -61,8 +61,8 @@ export const login = async (req, res, next) => {
 
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -79,8 +79,8 @@ export const logout = async (req, res, next) => {
   try {
     res.clearCookie("accessToken", {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
     });
 
     return res.status(200).json({
