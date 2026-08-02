5afa235083fa73731c48f25b87ed4c95c3e42f5a
Abdul-Kalam0 <abdulkalamblycomp@gmail.com>
2026-08-02 16:38:26 +0530

    feat(frontend): update loading

--- frontend/src/routes/PublicRoute.jsx
+++ frontend/src/routes/PublicRoute.jsx
@@ -13,7 +13,9 @@ const PublicRoute = () => {
   if (loading) {
     return (
       <div className="flex h-screen items-center justify-center">
-        <div className="h-12 w-12 animate-spin rounded-full border-4 border-gray-300 border-t-blue-600"></div>
+        <div className="h-12 w-12 animate-spin rounded-full border-4 border-gray-300 border-t-blue-600">
+          Hii
+        </div>
       </div>
     );
   }
